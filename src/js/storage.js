a7f38bfc7efd05a26a1cf7723f527da4f07e408a
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-06-16 00:45:11 +0200

    поудалял лишний код

--- src/js/storage.js
+++ src/js/storage.js
@@ -29,28 +29,3 @@ function onSwitchTheme() {
     refs.body.setAttribute('class', localStorage.getItem('LIGHT'));
   }
 }
-
-// const body = document.querySelector('body');
-// const themeSwitch = document.querySelector('.theme-switch__toggle');
-
-// themeSwitch.addEventListener('change', onSwitchTheme);
-
-// if (localStorage.getItem('DARK')) {
-//   body.setAttribute('class', localStorage.getItem('DARK'));
-// themeSwitch.checked = true;
-// } else if (localStorage.getItem('LIGHT')) {
-//   body.setAttribute('class', localStorage.getItem('LIGHT'));
-//   themeSwitch.checked = false;
-// }
-
-// function onSwitchTheme() {
-//   if (themeSwitch.checked) {
-//     localStorage.setItem('DARK', Theme.DARK);
-//     localStorage.removeItem('LIGHT');
-//     body.setAttribute('class', localStorage.getItem('DARK'));
-//   } else {
-//     localStorage.setItem('LIGHT', Theme.LIGHT);
-//     localStorage.removeItem('DARK');
-//     body.setAttribute('class', localStorage.getItem('LIGHT'));
-//   }
-// }
